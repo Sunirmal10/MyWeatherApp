f215352a45f64a35cbdca51959c540767c3890b7
Sunirmal Das <sunirmald1996@gmail.com>
2023-02-01 13:53:01 +0530

    complete weather-app

--- app.js
+++ app.js
@@ -14,31 +14,28 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function (req, res) {
 
-     res.sendFile(__dirname + "/index.html")     
+     res.sendFile(__dirname + "/index.html");    
 });
+
 
 app.post("/", function(req, res){
     console.log(req.body.cityName);
 
     const city = req.body.cityName;
 
-    // for lat & long inputs
-    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=` + apiKey + `&units=${unit}`;
-
-    
-
-    // for searching by city name
-
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=` + apiKey + `&units=${unit}`;
 
     https.get(url, function(response){
         console.log(response.statusCode);
+    let code = response.statusCode;
+    
+    if (code ==200) {
     
     response.on("data", function(data){
       let wData = JSON.parse(data)
       console.log(wData);
 
-      let temp = wData.main.temp;
+      let temp = wData.main.temp ;
       let cityname = wData.name;
       let country = wData.sys.country;
       let descrip = wData.weather[0].description;
@@ -50,40 +47,27 @@ app.post("/", function(req, res){
 
       let img_url =  "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-     
-
-
-      res.render("abc",{wicon:img_url, tempa:temp, des: descrip, city: cityname, count:country, feel: feelsLike, humid: humidity},
-      
-    //   document.getElementsByClassName("btn").addEventListener("click", () => {
-    //     console.log('got clicked');
-    // })
-    
-
-      );
-      
-    // res.write("<p>hello</p>");
-    //   res.write(`${cityname} `);
-    //   res.write(`${country} `);
-    //   res.write(`${temp} degree C`);
-    //   res.write("<img src=" + img_url + ">");
-    //   res.send();
-
-
-     
-
+      res.render("weatherData",{wicon:img_url, tempa:temp, des: descrip, city: cityname, count:country, feel: feelsLike, humid: humidity});
 
     })
+}
+else {
+    res.sendFile(__dirname + "/error.html");
+}
     })
     
 });
 
 app.post("/redirect", (req, res)=>{
     res.redirect("/");
-})
+});
+
+app.post("/home", (req, res)=>{
+    res.redirect("/");
+});
 
 
 app.listen(2000, function (){
-    console.log("server is running");
+    console.log("server is running on 2000");
 }
 );
